475898e87bc52f600a739b3924bc1b2899b84588
Francesco <franc.ns@gmail.com>
2020-01-22 13:51:48 +0100

    final

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+use App\Http\Requests\CategoryRequest;
+use App\Category;
+use App\Post;
+
+class MainController extends Controller
+{
+    public function index() {
+        $categories = Category::all();
+        return view('pages.index', compact('categories'));
+    }
+
+    public function postShow($id) {
+        $post = Post::findOrFail($id);
+        return view('pages.postShow', compact('post'));
+    }
+
+    public function postEdit($id) {
+        $post = Post::findOrFail($id);
+        return view('pages.postEdit', compact('post'));
+    }
+
+    public function postUpdate(PostRequest $request, $id) {
+        $validatedData = $request -> validated();
+        $post = Post::findOrFail($id);
+        $post -> update($validatedData);
+        return redirect() -> route('post.show', $id);
+    }
+
+    public function postDelete($id) {
+        $post = Post::findOrFail($id);
+        $post -> delete();
+        return redirect() -> route('home.index');
+    }
+
+    public function categoryEdit($id) {
+        $category = Category::findOrFail($id);
+        return view('pages.categoryEdit', compact('category'));
+    }
+
+    public function categoryUpdate(CategoryRequest $request, $id) {
+        $validatedData = $request -> validated();
+        $category = Category::findOrFail($id);
+        $category -> update($validatedData);
+        return redirect() -> route('home.index');
+    }
+
+    public function categoryDelete($id) {
+        $category = Category::findOrFail($id);
+        // $posts = $category->posts -> each(function($post) {
+        //     $post -> delete();
+        // });
+        $category -> posts() -> delete();
+        $category -> delete();
+        return redirect() -> route('home.index');
+    }
+
+    public function categoryPost($id) {
+        $category = Category::findOrFail($id);
+        return view('pages.categoryPost', compact('category'));
+    }
+
+    public function categoryPostCreate(PostRequest $request, $id) {
+        $validatedData = $request -> validated();
+        $post = Post::make($validatedData);
+        $category = Category::findOrFail($id);
+        $post -> category() -> associate($category);
+        $post -> save();
+        return redirect() -> route('home.index');
+    }
+
+}
